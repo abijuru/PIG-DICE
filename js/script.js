c6827d47b43b359e5a220e07b4ea5f2f37872915
abijuru <abijuruelisa@gmail.com>
2019-01-10 08:09:10 -0500

    add declaration of var game

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
+var scores, roundScore, activePlayer,gamePlaying;
+var btnRoll = document.querySelector('.btn-roll');
+var btnHold = document.querySelector('.btn-hold');
 
-$(document).ready(function (){
-   $('#blanks form').submit(function(event){
-     var marks=0;
-       var blanks=["answers1","answers2","answers3","answers4","answers5"]
-        blanks.forEach(function(blank) {
-           var userInput = $("input:radio[name=" + blank + "]:checked" ).val();
-               if(userInput=="A"){
-                   marks= marks+8;
-                   };
-                   
-            
-                  
-                   });                    
-                   alert("total: "+ marks + "/40" );                
-                   event.preventDefault();
-            });
-            });
 
