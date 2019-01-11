deab42d0118fa40fdba78f9730189acc38c03876
abijuru <abijuruelisa@gmail.com>
2019-01-11 06:02:04 -0500

    declare roll of dice using math round

--- js/script.js
+++ js/script.js
@@ -14,6 +14,17 @@ var Player = function(turn) {
     }
   };
 
-
+  Player.prototype.roll = function() {
+    var rand = Math.floor(Math.random() * 6) + 1;
+    this.currentScore = rand;
+  };
+  
+  Player.prototype.turnTotal = function(rollPoints) {
+    this.currentScore += rollPoints
+  }
+  
+  Player.prototype.addPoints = function(rollPoints) {
+    this.scoreTotal += rollPoints;
+  };
 
 
